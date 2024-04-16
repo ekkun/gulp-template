a9735443bbc014f0984dd38a25c0e72e9f566951
Eiji Kobayashi <eiji@design24c.com>
2024-04-16 17:27:11 +0900

    gulp_ejs_ts_sass: gulpfile まわり一式

---
+++ gulp_ejs_ts_sass/webpack.production.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const WebpackWatchedGlobEntries = require('webpack-watched-glob-entries-plugin');
+
+const entriesTS = WebpackWatchedGlobEntries.getEntries([path.resolve(__dirname, `./src/js/*.ts`)])();
+
+const App = {
+  mode: 'production', // production, development
+  entry: entriesTS,
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/public',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  target: ['web', 'es5'],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+          compress: {
+            drop_console: true,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+};
+
+module.exports = App; //実行
